b9998790868b65c556e1b6d8b458b4c204577d48
driehoeken <a.bednarski1203@gmail.com>
2023-06-13 22:04:59 +0200

    first commit, basic elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Timer from "./Timer/Timer";
+import Cases from "./Cases/Cases";
+import History from "./History/History";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Cases />
+            <Timer />
+            <History />
+        </>
+    );
 }
 
 export default App;
---
+++ src/Timer/Timer.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const Timer = () => {
+    return (
+        <div className="timer">
+            <div className="setup">Setup: B U2 L' F D' R2</div>
+            <div className="time"> 0.00</div>
+        </div>
+    );
+};
+
+export default Timer;
